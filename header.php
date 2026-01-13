0540330e5b0341f146922772d593d64bb8f26f64
Jotish <jotishk649@gmail.com>
2026-01-13 15:39:25 +0530

    changes

---
+++ header.php
@@ -0,0 +1,67 @@
+    <!-- header -->
+    <header class="header">
+        <!-- top header -->
+        <div class="header-top">
+            <div class="container">
+                <div class="header-top-wrapper">
+                    <div class="header-top-left">
+                        <div class="header-top-contact">
+                            <ul>
+                                <li class="mail-fix"><a href="mailto:1989GOPALAKRISHNAN06@gmail.com"><i
+                                            class="far fa-envelope"></i>
+                                        1989gopalakrishnan06@gmail.com</a></li>
+                                <li><a href="tel:+919790517650"><i class="far fa-phone-volume"></i> +91 97905 17650</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="header-top-right social-fix">
+                        <div class="header-top-social">
+                            <span>Follow Us: </span>
+                            <a><i class="fab fa-facebook"></i></a>
+                            <a><i class="fab fa-twitter"></i></a>
+                            <a><i class="fab fa-instagram"></i></a>
+                            <a><i class="fab fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-navigation">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container position-relative">
+                    <!-- Logo on left side -->
+                    <a class="navbar-brand" href="index.html">
+                        <span>Sivan Sakthi</span> Enterprises
+                    </a>
+                    <div class="mobile-menu-right">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-mobile-icon"><i class="far fa-bars"></i></span>
+                        </button>
+                    </div>
+                    <!-- Updated header menu without search -->
+                    <div class="collapse navbar-collapse" id="main_nav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
+                            <li class="nav-item"><a class="nav-link" href="products.html">Products</a></li>
+                            <li class="nav-item"><a class="nav-link" href="gallery.html">Gallery</a></li>
+                            <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                        </ul>
+                        <div class="nav-right" style="margin-left: 50px;">
+                            <div class="nav-right-btn mt-2 ">
+                                <a href="tel:+919790517650" class="theme-btn" style="color: FFB300;"><span
+                                        class="fas fa-phone"></span>Call
+                                    Now</a>
+                            </div>
+                            <div class="sidebar-btn">
+                                <button type="button" class="nav-right-link"><i class="far fa-bars-filter"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <!-- header end -->
